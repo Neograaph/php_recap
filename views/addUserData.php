0e908c6d6afba95e07b7deecf1e155d09a090a34
Maxime Gauthier <max45270@gmail.com>
2021-09-11 19:45:48 +0200

    'addUserData'

--- views/addUserData.php
+++ views/addUserData.php
@@ -1,30 +1,39 @@
 <?php
-if (isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['password'])){
-    $pseudo = strip_tags($_POST['pseudo'],"<br/>");
-    $email = strip_tags($_POST['email'],"<br/>");
-    $password = strip_tags($_POST['password'],"<br/>");
-
-    if ((!empty($pseudo)) && (!empty($email)) && (!empty($password))){
-      echo $pseudo.'<br/>';
-      echo $email.'<br/>';
-      echo $password.'<br/>';
-
-    }
-else{
-  echo 'no post var';
-};
-}
-?>
-
-<?php
-  try {
-    $bdd = new PDO('mysql:host=localhost;dbname=viabet;charset=utf8', 'root', '');
-    echo "connexion à la base de données réussie";
-  }
-  catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-  }
-  $response=$bdd->query('SELECT*FROM jeux_video ORDER BY nom');
+if (isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['mdp'])){
+  $pseudo = strip_tags($_POST['pseudo'],"<br/>");
+  $email = strip_tags($_POST['email'],"<br/>");
+  $mdp = strip_tags($_POST['mdp'],"<br/>");
   
-  $response->closeCursor();
+  if ((!empty($pseudo)) && (!empty($email)) && (!empty($mdp))){
+    echo $pseudo.'<br/>';
+    echo $email.'<br/>';
+    echo $mdp.'<br/>';
+    $dateNow = date('y-m-d');
+    echo $dateNow.'<br/>';
+  }
+  else{
+    echo 'no post var';
+  };
+  // $response->closeCursor();
+}
+
+
+try {
+  $bdd = new PDO('mysql:host=localhost;dbname=php_form;charset=utf8', 'root', '');
+  echo "connexion à la base de données réussie <br/>";
+}
+catch (Exception $e) {
+  die('Erreur : ' . $e->getMessage());
+}
+
+$req = $bdd->prepare("INSERT INTO users(id, pseudo, email, mdp, created_at) VALUES(:id, :pseudo, :email, :mdp, :created_at)");
+$req->execute(array(
+  'id' => '',
+  'pseudo' => $pseudo,
+  'email' => $email,
+  'mdp' => $mdp,
+  'created_at' => $dateNow
+));
+echo 'utilisateur ajouté à la bdd';
+
 ?>
--- views/layout/formAddUser.php
+++ views/layout/formAddUser.php
@@ -5,7 +5,7 @@
   <form action="./addUserData.php" class='d-flex gap-2' method='post'>
     <input class='form-control' name='pseudo' placeholder='pseudo' type="text" required autofocus autocomplete='off'>
     <input class='form-control' name='email' placeholder='email' type="email" required>
-    <input class='form-control' name='password' placeholder='password' type="password" required>
+    <input class='form-control' name='mdp' placeholder='password' type="password" required>
     <input class='btn btn-success' type="submit">
   </form>
 </div>
