95154b938946a6f5ab981fa9e12ad616cbd55f15
Maxime Gauthier <max45270@gmail.com>
2021-09-16 16:39:45 +0200

    addVueJS

--- views/layout/head.php
+++ views/layout/head.php
@@ -9,5 +9,6 @@
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css' integrity='sha512-F7WyTLiiiPqvu2pGumDR15med0MDkUIo5VTVyyfECR5DZmCnDhti9q5VID02ItWjq6fvDfMaBaDl2J3WdL1uxA==' crossorigin='anonymous'/>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.9.0/css/mdb.min.css' integrity='sha512-m3EYKL4iarW1aTPketC1NBARv57aOn6M7VeROdskWNkHptxOcM3w/zWz84jvS8u7chF+MFV7pposSLkVprwT9w==' crossorigin='anonymous'/>
   <link rel="stylesheet" href="./public/style/hero.css">
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.9/vue.global.min.js' integrity='sha512-Q1ILNdqt5Lsl3UVJaq5c+48VJQKSHkFasccOXsmmLPgS1o6ZjQS5qmQpy26vbftEFdi+qTobc40+9MilCya4cg==' crossorigin='anonymous'></script>
 </head>
 <body>
