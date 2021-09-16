3b797a2de9202eb04051518808c634c295a85de2
Maxime Gauthier <max45270@gmail.com>
2021-09-16 20:36:22 +0200

    addChanForum

--- views/forum.php
+++ views/forum.php
@@ -1,6 +1,23 @@
 <?php
 session_start();
 $pseudo = $_SESSION['pseudo'];
+
+$servername = "maximefneograph.mysql.db";
+$username = "maximefneograph";
+$password = "6WtuCxrP7ygy";
+$dbname = "maximefneograph";
+
+try {
+  $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // echo "connexion à la base de données réussie <br/>";
+}
+catch (PDOException $e) {
+  die('Erreur : ' . $e->getMessage());
+}
+
+
 include('./layout/head.php');
 ?>
 <?php
@@ -8,7 +25,30 @@ include('./layout/navPrivate.php');
 ?>
 <section id="app">
   <h1 class='text-center m-3'>{{ title }}</h1>
-  
+  <table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Salon</th>
+      <th scope="col">Création</th>
+      <th scope="col">Rejoindre</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+      $req=$bdd->prepare("SELECT * FROM php_recap_chan");
+      $req->execute();
+      foreach ($req as $donnees) {
+        echo '<tr>';
+        echo '<td>' . $donnees['id']. '</td>';
+        echo '<td>' . $donnees['title']. '</td>';
+        echo '<td>' . $donnees['created_at']. '</td>';
+        echo '<td>' . "<a class='btn btn-danger'>JOIN</a>". '</td>';
+        echo '</tr>';
+      }
+  ?>
+  </tbody>
+</table>
 
 
 
