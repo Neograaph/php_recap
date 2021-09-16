9f015596b614fcabe27d9efcb06ad30c8597c638
Maxime Gauthier <max45270@gmail.com>
2021-09-16 15:48:35 +0200

    modifsForms

--- views/layout/formAddUser.php
+++ views/layout/formAddUser.php
@@ -2,11 +2,15 @@
   <div>
     <h2>Ajouter un utilisateur</h2>
   </div>
-  <form action="./addUserData.php" class='d-flex gap-2' method='post' id='addFormUser'>
+  <form action="./addUserData.php" class='d-flex gap-2 flex-column' method='post' id='addFormUser'>
+  <div class='d-flex gap-2'>
     <input class='form-control' name='pseudo' placeholder='pseudo' type="text" required autofocus autocomplete='off'>
     <input class='form-control' name='email' placeholder='email' type="email" required>
     <input class='form-control' name='mdp' placeholder='password' type="password" required>
+  </div>
+  <div class='d-flex justify-content-center'>
     <input class='btn btn-success' type="submit" value='Inscription'>
+  </div>
   </form>
 </div>
 <div class='d-flex justify-content-center mt-5'>
--- views/layout/formConnexion.php
+++ views/layout/formConnexion.php
@@ -2,14 +2,18 @@
   <div>
     <h2>Se connecter</h2>
   </div>
-  <form action="./data.php" class='d-flex gap-2' method='post' id='formConnexion'>
-    <input class='form-control' name='pseudo' placeholder='pseudo' type="text" required autofocus autocomplete='off'>
-    <input class='form-control' name='mdp' placeholder='password' type="password" required>
-    <input class='btn btn-success' type="submit" value='Connexion'>
+  <form action="./data.php" class='d-flex gap-2 flex-column' method='post' id='formConnexion'>
+    <div class='d-flex gap-2'>
+      <input class='form-control' name='mdp' placeholder='password' type="password" required>
+      <input class='form-control' name='pseudo' placeholder='pseudo' type="text" required autofocus autocomplete='off'>
+    </div>
+    <div class='d-flex justify-content-center'>
+      <input class='btn btn-success' type="submit" value='Connexion'>
+    </div>
   </form>
 </div>
 <div class='d-flex justify-content-center mt-5'>
-  <a href="../index.php">
-    <button class='btn btn-danger'>Retour</button>
+  <a href="../index.php" class='btn btn-danger'>
+    Retour
   </a>
 </div>
