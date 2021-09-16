37e55613cacd0655db6e5136805a8c3f4025179a
Maxime Gauthier <max45270@gmail.com>
2021-09-16 16:01:54 +0200

    clean

--- views/layout/formConnexion.php
+++ views/layout/formConnexion.php
@@ -4,8 +4,8 @@
   </div>
   <form action="./data.php" class='d-flex gap-2 flex-column' method='post' id='formConnexion'>
     <div class='d-flex gap-2'>
-      <input class='form-control' name='mdp' placeholder='password' type="password" required>
       <input class='form-control' name='pseudo' placeholder='pseudo' type="text" required autofocus autocomplete='off'>
+      <input class='form-control' name='mdp' placeholder='password' type="password" required>
     </div>
     <div class='d-flex justify-content-center'>
       <input class='btn btn-success' type="submit" value='Connexion'>
